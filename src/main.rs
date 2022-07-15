88c8b58693b4dae14f6f1e2ba0eba22e5a523d82
Zachary S <zasample18+github@gmail.com>
2022-07-15 13:28:40 -0500

    Refactor image generation to a module.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::{
     cell::{Cell, RefCell},
-    ops::ControlFlow,
     rc::Rc,
     str::FromStr,
 };
@@ -17,133 +16,10 @@ use gtk::{
     Application, Builder, CssProvider, DrawingArea, EditableSignals, Entry, Inhibit, StyleContext,
     Window, Button,
 };
-use image::{Rgb, RgbImage};
-use num_complex::Complex;
+use image::RgbImage;
 
-#[derive(Debug, Clone)]
-struct Config {
-    max_iterations: u32,
-    center: (f64, f64),
-    zoom: f64,
-    hue_scale: f64,
-}
-
-impl Default for Config {
-    fn default() -> Self {
-        Self {
-            max_iterations: 1000,
-            center: (-0.75, 0.0),
-            zoom: 192.0,
-            hue_scale: 60.0,
-        }
-    }
-}
-
-// impl Config {
-//     fn parse() -> Result<Self, String> {
-//         let mut config = Config::default();
-//         let mut args = std::env::args();
-//         let _executable = args.next();
-//         while let Some(arg) = args.next() {
-//             match &*arg {
-//                 "-g" | "--gui" => {
-//                     config.gui = true;
-//                 }
-//                 "-o" => {
-//                     config.filename = Some(unwrap_or!(
-//                         args.next(),
-//                         return Err(format!("Expected argument for -o option")),
-//                     ));
-//                 }
-//                 "-i" => {
-//                     let iterations = unwrap_or!(
-//                         args.next(),
-//                         return Err(format!("Expected argument for -o option")),
-//                     );
-//                     config.max_iterations = unwrap_or!(
-//                         iterations.parse::<u32>(),
-//                         return Err(format!("Invalid number of iterations: {e:?}")),
-//                         with_error(e)
-//                     );
-//                 }
-//                 arg if arg.contains('x') => {
-//                     let idx = arg.find("x").expect("contains 'x'");
-//                     let (w, h) = arg.split_at(idx);
-//                     let h = &h[1..];
-//                     let w = unwrap_or!(
-//                         w.parse::<u32>(),
-//                         return Err(format!("Invalid width: {w:?}")),
-//                     );
-//                     let h = unwrap_or!(
-//                         h.parse::<u32>(),
-//                         return Err(format!("Invalid height: {h:?}")),
-//                     );
-//                     config.width = w;
-//                     config.height = h;
-//                 }
-//                 _ => {
-//                     return Err(format!("Unexpected argument: {arg:?}"));
-//                 }
-//             }
-//         }
-//         Ok(config)
-//     }
-// }
-
-fn fraction_to_hue(x: f64) -> Rgb<u8> {
-    let x = x % 1.0;
-    let x = (x * 1530.0) as u32;
-    #[allow(overlapping_range_endpoints)]
-    match x {
-        0..=255 => Rgb([255, x as u8, 0]),
-        255..=510 => Rgb([(510 - x) as u8, 255, 0]),
-        510..=765 => Rgb([0, 255, (x - 510) as u8]),
-        765..=1020 => Rgb([0, (1020 - x) as u8, 255]),
-        1020..=1275 => Rgb([(x - 1020) as u8, 0, 255]),
-        1275..=1530 => Rgb([255, 0, (1530 - x) as u8]),
-        _ => unreachable!("x <= 1530"),
-    }
-}
-
-fn generate(config: &Config, width: i32, height: i32) -> RgbImage {
-    let &Config {
-        max_iterations,
-        center: (cx, cy),
-        zoom,
-        hue_scale,
-        ..
-    } = config;
-    let width = width.try_into().unwrap();
-    let height = height.try_into().unwrap();
-
-    let mut img = image::RgbImage::new(width, height);
-    for y in 0..height {
-        let im = cy + (y as f64 - height as f64 / 2.0) / zoom;
-        for x in 0..width {
-            let re = cx + (x as f64 - width as f64 / 2.0) / zoom;
-            let c = Complex { re, im };
-            let f = |z: Complex<f64>| z * z + c;
-            let iterations =
-                (0..max_iterations).try_fold(Complex::new(0.0, 0.0), |val, iteration| {
-                    let val = f(val);
-                    if val.norm_sqr() > 4.0 {
-                        ControlFlow::Break(iteration)
-                    } else {
-                        ControlFlow::Continue(val)
-                    }
-                });
-
-            *img.get_pixel_mut(x, y) = match iterations {
-                ControlFlow::Break(it) => {
-                    fraction_to_hue((it as f64 / max_iterations as f64) * hue_scale)
-                }
-                ControlFlow::Continue(_) => image::Rgb([0, 0, 0]),
-            };
-        }
-    }
-
-    img
-}
+mod generation;
+use generation::{Config, generate};
 
 fn main() {
     let config = Config::default();
